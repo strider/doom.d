11968671b55822cc66dc28fc8b5e86461830012a
Gael Chamoulaud (Strider) <gchamoul@redhat.com>
2021-01-18 13:27:58 +0100

    Install ranger
    Signed-off-by: Gael Chamoulaud (Strider) <gchamoul@redhat.com>

--- packages.el
+++ packages.el
@@ -37,6 +37,9 @@
 (package! iedit
   :recipe (:host github :repo "victorhge/iedit"))
 
+(package! ranger
+  :recipe (:host github :repo "ralesi/ranger.el"))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
